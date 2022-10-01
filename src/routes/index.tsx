4ca16aeb20e2a58764af705ae493fb437114351b
justinrbeattie <justin.r.beattie@gmail.com>
2022-10-01 19:02:48 +1000

    change

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,34 @@
-
-
-import { component$, useServerMount$, useStore } from "@builder.io/qwik";
+import { component$, Resource, useResource$ } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
-
+import { getContent, RenderContent, getBuilderSearchParams } from "@builder.io/sdk-qwik";
 
 export const BUILDER_PUBLIC_API_KEY = "f5a098163c3741e19503f02a69360619";
 export const BUILDER_MODEL = "page";
 
-
-
 export default component$(() => {
   const location = useLocation();
-  const store = useStore({ data: {id:'',name:'', html:''} });
-
-  useServerMount$(async () => {
-    const response = await fetch('https://cdn.builder.io/api/v1/qwik/page?url='+ location.pathname +'&apiKey=f5a098163c3741e19503f02a69360619&limit=1');
-    store.data = await response.json();
+  const builderContentRsrc = useResource$<any>(() => {
+    return getContent({
+      model: BUILDER_MODEL,
+      apiKey: BUILDER_PUBLIC_API_KEY,
+      options: getBuilderSearchParams(location.query),
+      userAttributes: {
+        urlPath: location.pathname || "/",
+      },
+    });
   });
 
   return (
-    <>
-    <div dangerouslySetInnerHTML={store.data.html} />
-    </>
+    <Resource
+      value={builderContentRsrc}
+      onPending={() => <div>Loading...</div>}
+      onResolved={(content) => (
+        <RenderContent
+          model={BUILDER_MODEL}
+          content={content}
+          apiKey={BUILDER_PUBLIC_API_KEY}
+        />
+      )}
+    />
   );
 });
