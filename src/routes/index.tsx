02704bfd0a4ef9ccff6ac38341f20d58fca5ccfb
justinrbeattie <justin.r.beattie@gmail.com>
2022-10-02 13:46:26 +1100

    edit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,62 +1,24 @@
-import {
-  component$,
-  Resource,
-  useResource$,
-  useServerMount$,
-  useStore,
-} from "@builder.io/qwik";
+import { component$, useServerMount$, useStore } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
-import {
-  getBuilderSearchParams,
-  getContent,
-  RenderContent,
-} from "@builder.io/sdk-qwik";
 
 export const BUILDER_PUBLIC_API_KEY = "f5a098163c3741e19503f02a69360619";
 export const BUILDER_MODEL = "page";
 
 export default component$(() => {
   const location = useLocation();
-  const iFrameDetection = (window === window.parent) ? false : true;
-  if (iFrameDetection === false) {
-    const store = useStore({ resp: {  html: "" } });
-    const apiUrl =  "https://cdn.builder.io/api/v1/qwik/" +
-    BUILDER_MODEL +
-    "?url=https%3A%2F%2Fcosmic-dusk-68b932.netlify.app%2F&apiKey=" +
-    BUILDER_PUBLIC_API_KEY +
-    "&limit=1&userAttributes.urlPath=" +
-    location.pathname || "/";
+  const store = useStore({ resp: { html: "" } });
+  const apiUrl =
+    "https://cdn.builder.io/api/v1/qwik/" +
+      BUILDER_MODEL +
+      "?url=https%3A%2F%2Fcosmic-dusk-68b932.netlify.app%2F&apiKey=" +
+      BUILDER_PUBLIC_API_KEY +
+      "&limit=1&userAttributes.urlPath=" +
+      location.pathname || "/" + location.query;
 
-    useServerMount$(async () => {
-      const response = await fetch(apiUrl);
-      store.resp = await response.json();
-    });
+  useServerMount$(async () => {
+    const response = await fetch(apiUrl);
+    store.resp = await response.json();
+  });
 
-    return <div dangerouslySetInnerHTML={store.resp.html} />;
-  } else {
-    const builderContentRsrc = useResource$<any>(() => {
-      return getContent({
-        model: BUILDER_MODEL,
-        apiKey: BUILDER_PUBLIC_API_KEY,
-        options: getBuilderSearchParams(location.query),
-        userAttributes: {
-          urlPath: location.pathname || "/",
-        },
-      });
-    });
-
-    return (
-      <Resource
-        value={builderContentRsrc}
-        onPending={() => <div>Loading...</div>}
-        onResolved={(content) => (
-          <RenderContent
-            model={BUILDER_MODEL}
-            content={content}
-            apiKey={BUILDER_PUBLIC_API_KEY}
-          />
-        )}
-      />
-    );
-  }
+  return <div dangerouslySetInnerHTML={store.resp.html} />;
 });
