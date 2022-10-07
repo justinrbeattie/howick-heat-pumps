8876a58a310e98d8c47b3e41a0a3c98281be57d1
justinrbeattie <justin.r.beattie@gmail.com>
2022-10-07 17:03:56 +1100

    webcomp

---
+++ public/webcomponents.js
@@ -0,0 +1,24 @@
+class TextContent extends HTMLDivElement {
+    resizeObserver;
+    constructor() {
+      super();
+      this.adjustGridRows();
+      this.resizeObserver = new ResizeObserver((entries) => {
+        for (const entry of entries) {
+          this.adjustGridRows();
+        }
+      });
+      this.resizeObserver.observe(this);
+    }
+    disconnectedCallback() {
+      this.resizeObserver.disconnect();
+    }
+    adjustGridRows() {
+      this.parentElement.style.setProperty(
+        "--grid-span", Math.ceil(this.offsetHeight / (window.innerHeight / 10))
+      );
+    }
+  }
+  customElements.define("text-content", TextContent, {
+    extends: "div"
+  });
